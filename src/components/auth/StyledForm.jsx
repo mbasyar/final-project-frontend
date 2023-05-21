1163085df86df45a2d64fbcd2870f5c5e388935a
mbasyar <muhammadkhoirulb@gmail.com>
2023-05-21 15:59:59 +0700

    fixing

--- src/components/auth/StyledForm.jsx
+++ src/components/auth/StyledForm.jsx
@@ -3,10 +3,15 @@ import styled from "styled-components";
 export const StyledForm = styled.form`
   max-width: 350px;
   width: 100%;
-  margin: 2rem auto;
+  margin: 5rem auto;
+  border: 1px solid rgb(0, 208, 255);
+  border-radius: 5px;
+  padding: 5rem;
+  background-color: wheat;
 
   h2 {
     margin-bottom: 1rem;
+    color: blue;
   }
 
   button,
@@ -16,14 +21,16 @@ export const StyledForm = styled.form`
     padding: 7px;
     outline: none;
     border-radius: 5px;
-    border: 1px solid rgb(220, 220, 220);
+    border: 1px solid rgb(0, 208, 255);
     margin-bottom: 1rem;
     &:focus {
-      border: 1px solid rgb(0, 208, 255);
+      border: 1px solid rgb(0, 218, 245);
     }
   }
 
   button {
+    background-color: blue;
+    color: white;
     cursor: pointer;
     &:focus {
       border: none;
